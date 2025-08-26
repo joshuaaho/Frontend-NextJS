ee13c8a0e4a2f0e4ed88183a2fdb17a017219de4
Joshua <joshuaa@connect.hku.hk>
2025-08-26 16:55:02 +0800

    feat: add fields to bubble tea table

--- dexie/db.ts
+++ dexie/db.ts
@@ -1,15 +1,23 @@
-import Dexie, { type Table } from 'dexie';
+import Dexie, { type Table } from "dexie";
 
-export const db = new Dexie('BubbleTeaDB') as Dexie & {
+export const db = new Dexie("BubbleTeaDB") as Dexie & {
   bubbleTeas: Table<BubbleTea, number>;
 };
 
 db.version(1).stores({
-  bubbleTeas: '++id, name, isListed',
+  bubbleTeas: "++id, name, isListed",
 });
 
 export interface BubbleTea {
   id: number;
   name: string;
   isListed: boolean;
+
+  // TODO: Add more fields
+  price: number;
+  assetPath: string;
+  description: string;
+  currency: string;
+  quantity: number;
+  labels: string[];
 }
