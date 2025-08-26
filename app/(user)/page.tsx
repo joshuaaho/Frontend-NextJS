c1508c12fcad36127f8b6bab7f984179b51524df
Joshua <joshuaa@connect.hku.hk>
2025-08-26 18:55:23 +0800

    feat: show shopping cart list when clicking button

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,32 +3,38 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BubbleTeaService } from '../(services)/bubbleTeaService';
 import ProductList from './components/ProductList';
+import { ProductsProvider } from './contexts/ProductsContext';
+import ShoppingCartList from './components/ShoppingCartList';
 
 export default function UserPage() {
   useEffect(() => {
     BubbleTeaService.insertIfEmpty();
   }, []);
 
+  const [inShoppingCart, setInShoppingCart] = useState(false);
   return (
-    <Container>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Bubble Teas
-          </Typography>
-          <Button
-            variant="outlined"
-            endIcon={<ShoppingCartIcon />}
-            sx={{ borderColor: 'white', color: 'white' }}
-          >
-            Cart
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <ProductList />
-    </Container>
+    <ProductsProvider>
+      <Container>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Bubble Teas
+            </Typography>
+            <Button
+              variant="outlined"
+              endIcon={<ShoppingCartIcon />}
+              sx={{ borderColor: 'white', color: 'white' }}
+              onClick={() => setInShoppingCart(prev => !prev)}
+            >
+              Cart
+            </Button>
+          </Toolbar>
+        </AppBar>
+        {inShoppingCart ? <ShoppingCartList /> : <ProductList />}
+      </Container>
+    </ProductsProvider>
   );
 }
