c93d67bf7ee47ee309858ac1f6a07e36c18c2d3c
Joshua <joshuaa@connect.hku.hk>
2025-08-26 17:08:40 +0800

    test: fix test

--- app/(user)/components/productCard/productPrice/ProductPrice.test.tsx
+++ app/(user)/components/productCard/productPrice/ProductPrice.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import ProductPrice from "./ProductPrice";
 
 test("ProductPrice", () => {
-  render(<ProductPrice priceView="32 HKD" />);
+  render(<ProductPrice price="32 HKD" />);
   expect(screen.getByText("32 HKD")).toBeDefined();
 });
 
