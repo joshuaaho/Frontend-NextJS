1b6e1e156c4bc9c5485b729b270544c2de1612b9
Joshua <joshuaa@connect.hku.hk>
2025-08-27 14:35:20 +0800

    refactor: use better test descriptions

--- app/(user)/components/productCard/Name/Name.test.tsx
+++ app/(user)/components/productCard/Name/Name.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Name from "./Name";
 
-test("Name", () => {
+test("Name renders the name text", () => {
   render(<Name name="Test Bubble Tea" />);
   expect(screen.getByText("Test Bubble Tea")).toBeDefined();
 });
--- app/(user)/components/productCard/Price/Price.test.tsx
+++ app/(user)/components/productCard/Price/Price.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Price from "./Price";
 
-test("Price", () => {
+test("Price renders the price text", () => {
   render(<Price price="32 HKD" />);
   expect(screen.getByText("32 HKD")).toBeDefined();
 });
