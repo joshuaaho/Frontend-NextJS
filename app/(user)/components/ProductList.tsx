8197253a91d778cb7f9086aaf4164d914998c145
Joshua <joshuaa@connect.hku.hk>
2025-08-27 13:52:44 +0800

    feat: fix styling issues

--- app/(user)/components/ProductList.tsx
+++ app/(user)/components/ProductList.tsx
@@ -7,7 +7,8 @@ import { Container, Box, Typography } from "@mui/material";
 import Card from "./ProductCard/Card";
 
 const ProductList = () => {
-  const { products, labels, handleAddToCart, handleRemoveFromCart } = useProducts();
+  const { products, labels, handleAddToCart, handleRemoveFromCart } =
+    useProducts();
 
   return (
     <Container
@@ -20,40 +21,42 @@ const ProductList = () => {
         alignItems: "flex-start",
       }}
     >
-      {sortLabels(Object.keys(labels))
-        .map((label) => (
-          <Box key={label} sx={{ width: "100%", marginBottom: 2 }}>
-            <Typography
-              variant="h4"
-              sx={{
-                fontSize: "1.5rem",
-                fontWeight: "bold",
-                marginBottom: 1,
-                color: "#333",
-              }}
-            >
-              {formatLabel(label)}
-            </Typography>
-            <Box
-              sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                gap: 2,
-                justifyContent: "flex-start",
-              }}
-            >
-              {labels[label].map((productId: number) => (
-                <Box key={productId}>
-                  <Card
-                    bubbleTea={products[productId]}
-                    handleAddToCart={handleAddToCart}
-                    handleRemoveFromCart={handleRemoveFromCart}
-                  />
-                </Box>
-              ))}
-            </Box>
+      {sortLabels(Object.keys(labels)).map((label) => (
+        <Box key={label} sx={{ width: "100%", marginBottom: 2 }}>
+          <Typography
+            variant="h4"
+            sx={{
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+              marginBottom: 1,
+              color: "#333",
+            }}
+          >
+            {formatLabel(label)}
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: 2,
+              justifyContent: "flex-start",
+            }}
+          >
+            {labels[label].map(
+              (productId: number) =>
+                products[productId].isListed && (
+                  <Box key={productId}>
+                    <Card
+                      bubbleTea={products[productId]}
+                      handleAddToCart={handleAddToCart}
+                      handleRemoveFromCart={handleRemoveFromCart}
+                    />
+                  </Box>
+                )
+            )}
           </Box>
-        ))}
+        </Box>
+      ))}
     </Container>
   );
 };
