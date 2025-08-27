2493aaede9d55e6ee6a32631cb96d8fcea2488a5
Joshua <joshuaa@connect.hku.hk>
2025-08-27 13:50:55 +0800

    feat: allow switching between cart and home

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,40 +1,66 @@
-'use client';
+"use client";
 
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
-import Container from '@mui/material/Container';
-import { useEffect, useState } from 'react';
-import { BubbleTeaService } from '../(services)/bubbleTeaService';
-import ProductList from './components/ProductList';
-import { ProductsProvider } from './contexts/ProductsContext';
-import ShoppingCartList from './components/ShoppingCartList';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import {
+  Alert,
+  AppBar,
+  Button,
+  Snackbar,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Container from "@mui/material/Container";
+import { useEffect, useState } from "react";
+import { BubbleTeaService } from "../(services)/bubbleTeaService";
+import ProductList from "./components/ProductList";
+import { ProductsProvider } from "./contexts/ProductsContext";
+import ShoppingCartList from "./components/ShoppingCartList";
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function UserPage() {
   useEffect(() => {
     BubbleTeaService.insertIfEmpty();
   }, []);
 
+  const [openToast, setOpenToast] = useState(false);
+
   const [inShoppingCart, setInShoppingCart] = useState(false);
   return (
-    <ProductsProvider>
-      <Container>
-        <AppBar position="fixed">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Bubble Teas
-            </Typography>
-            <Button
-              variant="outlined"
-              endIcon={<ShoppingCartIcon />}
-              sx={{ borderColor: 'white', color: 'white' }}
-              onClick={() => setInShoppingCart(prev => !prev)}
-            >
-              Cart
-            </Button>
-          </Toolbar>
-        </AppBar>
-        {inShoppingCart ? <ShoppingCartList /> : <ProductList />}
-      </Container>
-    </ProductsProvider>
+    <Container>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Bubble Teas
+          </Typography>
+          <Button
+            variant="outlined"
+            endIcon={inShoppingCart ? <ShoppingCartIcon /> : <HomeIcon />}
+            sx={{ borderColor: "white", color: "white" }}
+            onClick={() => setInShoppingCart((prev) => !prev)}
+          >
+            {inShoppingCart ? "Cart" : "Home"}
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <ProductsProvider>
+        {inShoppingCart ? (
+          <ShoppingCartList
+            setOpenToast={setOpenToast}
+            setInShoppingCart={setInShoppingCart}
+          />
+        ) : (
+          <ProductList />
+        )}
+      </ProductsProvider>
+      <Snackbar
+        open={openToast}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert severity="success" onClose={() => setOpenToast(false)}>
+          Order submitted successfully! Your cart has been cleared.
+        </Alert>
+      </Snackbar>
+    </Container>
   );
 }
