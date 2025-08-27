3078748d8c26bfa1c2d882ac33c38f637aba73e0
Joshua <joshuaa@connect.hku.hk>
2025-08-27 13:57:56 +0800

    feat: don't show quantity when 0 or less

--- app/(user)/components/productCard/ProductQuantity/ProductQuantity.tsx
+++ app/(user)/components/productCard/ProductQuantity/ProductQuantity.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Box } from '@mui/material';
 
 type ProductQuantityProps = {
   quantity: number;
 };
 
 export default function ProductQuantity({ quantity }: ProductQuantityProps) {
-  return <div>{`In cart: ${quantity}`}</div>;
+
+  if (quantity === 0) {
+    return null;
+  }
+
+  return (
+    <Box sx={{ 
+      minHeight: '24px', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'flex-end',
+      width: '100%'
+    }}>
+
+      {`In cart: ${quantity}`}
+      
+    </Box>
+  );
 }
