0922f3105b51df3fd07a772f30dbd80d821cade6
Joshua <joshuaa@connect.hku.hk>
2025-08-27 14:36:02 +0800

    chore: remove logs

--- app/(user)/utils/priceFormatter.test.ts
+++ app/(user)/utils/priceFormatter.test.ts
@@ -3,7 +3,7 @@ import { formatPrice } from "./priceFormatter";
 
 describe("priceFormatter", () => {
   test("should format price with currency", () => {
-    console.log(formatPrice(32, "HKD"), "ds");
+
     expect(formatPrice(32, "HKD")).toBe("32 HKD");
     expect(formatPrice(22, "USD")).toBe("22 USD");
     expect(formatPrice(0, "EUR")).toBe("0 EUR");
--- app/(user)/utils/productMap.ts
+++ app/(user)/utils/productMap.ts
@@ -2,7 +2,7 @@ export const createProductMap = (products: any[]) => {
   const productMap: any = {};
   for (const product of products) {
     productMap[product.id] = product;
-    console.log(product);
+
   }
   return productMap;
 };
