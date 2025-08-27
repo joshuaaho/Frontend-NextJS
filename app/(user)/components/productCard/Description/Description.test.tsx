6c7aa4c0f3032c63b95effe08e96683d375d55ac
Joshua <joshuaa@connect.hku.hk>
2025-08-27 14:37:38 +0800

    refactor: use better test description

--- app/(user)/components/productCard/Description/Description.test.tsx
+++ app/(user)/components/productCard/Description/Description.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Description from "./Description";
 
-test("Description", () => {
+test("Description renders the description text", () => {
   render(<Description description="Test Description" />);
   expect(screen.getByText("Test Description")).toBeDefined();
 }); 
